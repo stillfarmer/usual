9f3bd1abd23650630d74002f0e201c2ffb80951d
wudi <wudi_stillfarmer@163.com>
2015-08-29 16:36:06 +0800

    add software.sh

--- sh/init.sh
+++ sh/init.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
+    cp ./ssh.cmd ~/
     sudo apt-get update;
 
     sudo apt-get install vim subversion;
     sudo apt-get install cscope ctags;
     sudo apt-get install git;
     sudo apt-get install bison flex sharutils;
-
-    cp ./ssh.cmd ~/
+    ./software.sh;
 
