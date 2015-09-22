0d5b4bffff24da0cad26ff841e68992654081d0f
wudi <wudi_stillfarmer@163.com>
2015-09-22 09:11:36 +0800

    install build-essential, fix bug gnutls-handshake, A TLS packet with ...

---
+++ sh/build.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+    sudo apt-get install g++;
+    sudo apt-get install zlib1g-dev;
+    sudo apt-get install libncureses5-dev;
+    sudo apt-get install gawk;
+	sudo apt-get install build-essential;
+    
