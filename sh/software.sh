19b14466383fbf09c1dd78cfb9af9b6a17ff683d
wudi <di.wu@feixun.com.cn>
2015-10-29 10:31:14 +0800

    install tftp client

--- sh/software.sh
+++ sh/software.sh
@@ -3,6 +3,7 @@
     sudo apt-get install wireshark;
     sudo apt-get install minicom;
     sudo apt-get install tftpd-hpa;
+    sudo apt-get install tftp-hpa;
     sudo apt-get install openssh-server;
     sudo apt-get install vsftpd;
     sudo apt-get install meld;
